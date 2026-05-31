a219c22a0bf8b730ebf4686f01c137d3349e0f09
artyomdev250 <artyom.dev250@gmail.com>
2026-05-31 17:31:46 +0500

    get a single user

--- controllers/users/getUsers.js
+++ controllers/users/getUsers.js
@@ -2,6 +2,23 @@ const User = require("../../mongodb/models/User");
 
 const getUsers = async (req, res) => {
     try {
+        const { id } = req.params;
+
+        if (id) {
+            const user = await User.findById(id);
+
+            if (!user) {
+                return res.status(404).json({
+                    message: "User not found",
+                });
+            }
+
+            return res.status(200).json({
+                message: "User fetched successfully",
+                user,
+            });
+        }
+
         const users = await User.find();
 
         res.status(200).json({
@@ -11,7 +28,7 @@ const getUsers = async (req, res) => {
         });
     } catch (error) {
         res.status(500).json({
-            message: "Failed to get users",
+            message: "Failed to get user/users",
             error: error.message,
         });
     }
--- router/usersRoutes.js
+++ router/usersRoutes.js
@@ -8,6 +8,7 @@ const deleteUser = require("../controllers/users/deleteUser");
 const router = express.Router();
 
 router.get("/", getUsers);
+router.get("/:id", getUsers);
 router.post("/", createUser);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
